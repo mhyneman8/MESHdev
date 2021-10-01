ae41ff6fa613fa7288f5d565f5d85f3611eb2b87
mhyneman8 <mallory.hyneman@gmail.com>
2021-09-30 19:30:32 -0500

    form css

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// create observer
+const observer = new IntersectionObserver(entries => {
+//     // loop over the entries
+//     entries.forEach(entry => {
+//         if (entry.isIntersection) {
+//             // its visible. add animation class
+//             entry.target.classList.add('larger');
+//         }
+//     });
+// });
+
+// tell th eobserver which elements to track
+// observer.observe(document.querySelector('.sub-headings'));
+
